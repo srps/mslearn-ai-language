ede0f2818d819731b390585398ed53253eb0c697
Sérgio Pereira da Silva <sergio.pds@outlook.pt>
2023-12-18 14:11:49 +0000

    Complete Python Module 2
    Complete 01-qna-app in Python

--- Labfiles/02-qna/Python/qna-app/qna-app.py
+++ Labfiles/02-qna/Python/qna-app/qna-app.py
@@ -2,6 +2,8 @@ from dotenv import load_dotenv
 import os
 
 # Import namespaces
+from azure.core.credentials import AzureKeyCredential
+from azure.ai.language.questionanswering import QuestionAnsweringClient
 
 
 def main():
@@ -14,10 +16,19 @@ def main():
         ai_deployment_name = os.getenv('QA_DEPLOYMENT_NAME')
 
         # Create client using endpoint and key
+        credential = AzureKeyCredential(key=ai_key)
+        ai_client = QuestionAnsweringClient(endpoint=ai_endpoint, credential=credential)
 
 
         # Submit a question and display the answer
-
+        user_question = ''
+        while user_question.lower() != 'quit':
+            user_question = input('\nQuestion:\n')
+            response = ai_client.get_answers(project_name=ai_project_name, deployment_name=ai_deployment_name, question=user_question)
+            for candidate in response.answers:
+                print(candidate.answer)
+                print('Confidence: {}'.format(candidate.confidence))
+                print('Source: {}'.format(candidate.source))
 
 
     except Exception as ex:
